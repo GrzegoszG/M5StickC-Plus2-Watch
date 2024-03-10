ef5f6a86dc8bec9f2d488ca1fb5d1a91d402cd55
grzegosz <ggorkiew@gmail.com>
2024-03-10 23:10:27 +0100

    basic BLE advertising (disabled for now)

--- m5stickc_plus2_watch/m5stickc_plus2_watch.ino
+++ m5stickc_plus2_watch/m5stickc_plus2_watch.ino
@@ -16,6 +16,11 @@
 
 #include "M5StickCPlus2.h"
 
+// BLE
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
 #define MAX_VOLTAGE 4350
 #define MIN_VOLTAGE 3000
 #define TIMEZONE 1
@@ -35,6 +40,11 @@
 #define MODE_CLOCK_DRAW_DELAY 3000
 #define MODE_FLASHLIGHT_DRAW_DELAY 10000
 #define MODE_ALARM_DRAW_DELAY 1000
+
+
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b" // random uuid
+
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b2137" //random uuid
 
 unsigned int diceType;
 unsigned int diceCount;
@@ -127,6 +137,39 @@ void setup()
     holdSwitchTime.second = startTime.second;
     holdSwitchTime.day = startTime.day;
     holdSwitchTime.month = startTime.month;
+
+    //initBT();
+}
+
+void initBT()
+{
+  BLEDevice::init("M5Stick C PLUS 2");
+
+  BLEServer *pServer = BLEDevice::createServer();
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+  CHARACTERISTIC_UUID,
+  BLECharacteristic::PROPERTY_READ |
+  BLECharacteristic::PROPERTY_WRITE );
+
+  pCharacteristic->setValue("Hello World");
+  pService->start();
+
+  // BLEAdvertising *pAdvertising = pServer->getAdvertising(); // this still is working for backward compatibility
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+
+  pAdvertising->setScanResponse(true);
+
+  pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issue
+
+  pAdvertising->setMinPreferred(0x12);
+
+  BLEDevice::startAdvertising();
 }
 
 void getDevicesData()
@@ -239,7 +282,7 @@ void beepAlarm()
     if (curTime.second % 2 == 0 && alarmLastBeepSecond < curTime.second)
     {
       alarmLastBeepSecond = curTime.second;
-      StickCP2.Speaker.tone(8000, 200);
+      StickCP2.Speaker.tone(8000, 40);
     }
     if (alarmTime.hour != curTime.hour || alarmTime.minute != curTime.minute)
     {
