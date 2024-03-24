4d0af8226f78df03378bb5ed86f73f64bc966b69
grzegosz <ggorkiew@gmail.com>
2024-03-24 22:03:57 +0100

    btn states, btnPWR, roll mode

--- m5stickc_plus2_watch/m5stickc_plus2_watch.ino
+++ m5stickc_plus2_watch/m5stickc_plus2_watch.ino
@@ -20,6 +20,7 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <Preferences.h>
 
 #define MAX_VOLTAGE 4350
 #define MIN_VOLTAGE 3000
@@ -35,7 +36,7 @@
 #define MODE_CLOCK 0
 #define MODE_FLASHLIGHT 1
 #define MODE_ALARM 2
-//#define MODE_DICE 3
+#define MODE_ROLL 3
 
 #define MODE_CLOCK_DRAW_DELAY 3000
 #define MODE_FLASHLIGHT_DRAW_DELAY 10000
@@ -45,14 +46,27 @@
 #define IDLE_MILIS 5000
 #define CLOCK_FREQUENCY 80
 #define GET_BAT_ITERATIONS 5
+#define RTC_SEC_OFFSET 5
 
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b" // random uuid
 
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b2137" //random uuid
+#define DICE_TYPES_COUNT 9
+#define DICE_COUNT_MAX 6
+
+#define BTN_STATUS_NONE 0
+#define BTN_STATUS_HOLD 1
+#define BTN_STATUS_CLICKED 2
+#define BTN_STATUS_PRESSED 3
+#define BTN_STATUS_RELEASED 4
+
+#define BTNA 0
+#define BTNB 1
+#define BTNPWR 2
 
 unsigned int diceType;
 unsigned int diceCount;
-unsigned int * diceResult;
+int * diceResult;
 unsigned int diceSum;
 
 float normalized_max = (MAX_VOLTAGE - MIN_VOLTAGE)*1.0f;
@@ -66,7 +80,25 @@ int modes[] =
 {
   MODE_CLOCK,
   MODE_FLASHLIGHT,
-  MODE_ALARM
+  MODE_ALARM,
+  MODE_ROLL
+};
+
+int colors[] = 
+{
+  RED,
+  YELLOW,
+  GREEN,
+  BLUE,
+  CYAN,
+  MAGENTA,
+  WHITE,
+  ORANGE
+};
+
+int diceTypes[] = 
+{
+  2, 3, 4, 6, 8, 10, 12, 20, 100
 };
 
 struct Time
@@ -79,19 +111,12 @@ struct Time
 };
 
 Time startTime = {1,1,0,0,0};
-
 Time curTime = {1,1,0,0,0};
-
 Time modeChangeStart = {1,1,0,0,0};
-
 Time modeChangedTime = {1,1,0,0,0};
-
 Time drawTime = {1,1,0,0,0};
-
 Time alarmTime = {1,1,0,0,0};
-
 Time holdSwitchTime = {1,1,0,0,0};
-
 Time lastClickTime = {1,1,0,0,0};
 
 int modeChangeSeconds;
@@ -112,7 +137,62 @@ int prevBrightness;
 bool idle;
 bool redraw;
 int getBatCounter;
+int randCounter = 1000;
 // M5.BtnPWR.
+bool prevDeviceConnected;
+bool deviceConnected;
+
+int btnStatus[3] = 
+{
+  BTN_STATUS_NONE, BTN_STATUS_NONE, BTN_STATUS_NONE
+};
+
+int rollMode = 0;
+
+Preferences preferences;
+
+
+//Setup callbacks onConnect and onDisconnect
+class BLECallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  };
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+  }
+};
+
+class BLECharCallbacks : public BLECharacteristicCallbacks {
+  void onRead(BLECharacteristic* pCharacteristic, esp_ble_gatts_cb_param_t* param)
+  {
+
+  }
+
+  void onRead(BLECharacteristic* pCharacteristic)
+  {
+
+  }
+
+  void onWrite(BLECharacteristic* pCharacteristic, esp_ble_gatts_cb_param_t* param)
+  {
+
+  }
+
+  void onWrite(BLECharacteristic* pCharacteristic)
+  {
+
+  }
+
+  void onNotify(BLECharacteristic* pCharacteristic)
+  {
+
+  }
+
+  void onStatus(BLECharacteristic* pCharacteristic, Status s, uint32_t code)
+  {
+
+  }
+};
 
 void setup() 
 {
@@ -128,13 +208,15 @@ void setup()
     modeIndex = 0;
     auto cfg = M5.config();
     StickCP2.begin(cfg);
+    preferences.begin("app", false);
     StickCP2.Display.setRotation(3);
-    clockColor = RED;
+    clockColor = YELLOW;
     StickCP2.Display.setTextColor(clockColor);
     StickCP2.Display.setTextDatum(middle_center);
     StickCP2.Display.setTextFont(&fonts::Orbitron_Light_24);
     StickCP2.Display.setTextSize(1);
     auto st = StickCP2.Rtc.getDateTime();
+
     startTime.month = st.date.month;
     startTime.day = st.date.date;
     startTime.hour = ((st.time.hours)+TIMEZONE)%24;
@@ -160,19 +242,32 @@ void setup()
     lastClickTime.month = startTime.month;
     modeCount = sizeof(modes)/sizeof(modes[0]);
     setCpuFrequencyMhz(CLOCK_FREQUENCY);
-    //initBT();
+
+    if (preferences.getBool("useBLE") || true)
+    {
+      initBT();
+    }
+}
+
+int getRandom(int from, int to)
+{
+  int range = to - from;
+  int r = randCounter % range;
+  return r;
 }
 
 void initBT()
 {
   BLEDevice::init("M5Stick C PLUS 2");
   BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new BLECallbacks());
   BLEService *pService = pServer->createService(SERVICE_UUID);
   BLECharacteristic *pCharacteristic = pService->createCharacteristic(
   CHARACTERISTIC_UUID,
   BLECharacteristic::PROPERTY_READ |
   BLECharacteristic::PROPERTY_WRITE );
   pCharacteristic->setValue("Hello World");
+  pCharacteristic->setCallbacks(new BLECharCallbacks());
   pService->start();
   // BLEAdvertising *pAdvertising = pServer->getAdvertising(); // this still is working for backward compatibility
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
@@ -183,10 +278,15 @@ void initBT()
   BLEDevice::startAdvertising();
 }
 
+
 void getDevicesData()
 {
   StickCP2.update();
-
+  randCounter++;
+  if (randCounter >= 10000)
+  {
+    randCounter = 1000;
+  }
   --getBatCounter;
   if (getBatCounter <= 0)
   {
@@ -228,11 +328,26 @@ void getDevicesData()
   curTime.day = dt.date.date;
   curTime.month = dt.date.month;
 
-  bool btnBHold = StickCP2.BtnB.wasHold();
-  bool btnBPressed = StickCP2.BtnB.wasPressed();
-  bool btnBReleased = StickCP2.BtnB.wasReleased();
-  bool btnAPressed = StickCP2.BtnA.wasPressed();
-  if (btnBHold || btnBPressed || btnBReleased || btnAPressed)
+  btnStatus[BTNA] = M5.BtnA.wasHold() ? BTN_STATUS_HOLD
+                  //: M5.BtnA.wasClicked() ? BTN_STATUS_CLICKED
+                  : M5.BtnA.wasPressed() ? BTN_STATUS_PRESSED
+                  : M5.BtnA.wasReleased() ? BTN_STATUS_RELEASED
+                  : BTN_STATUS_NONE;
+  btnStatus[BTNB] = M5.BtnB.wasHold() ? BTN_STATUS_HOLD
+                  //: M5.BtnB.wasClicked() ? BTN_STATUS_CLICKED
+                  : M5.BtnB.wasPressed() ? BTN_STATUS_PRESSED
+                  : M5.BtnB.wasReleased() ? BTN_STATUS_RELEASED
+                  : BTN_STATUS_NONE;
+  btnStatus[BTNPWR] = 
+                    M5.BtnPWR.wasHold() ? BTN_STATUS_HOLD
+                  //: M5.BtnPWR.wasClicked() ? BTN_STATUS_CLICKED
+                  : M5.BtnPWR.wasPressed() ? BTN_STATUS_PRESSED
+                  : M5.BtnPWR.wasReleased() ? BTN_STATUS_RELEASED
+                  : BTN_STATUS_NONE;
+
+  if (btnStatus[BTNA] != BTN_STATUS_NONE ||
+      btnStatus[BTNB] != BTN_STATUS_NONE ||
+      btnStatus[BTNPWR] != BTN_STATUS_NONE)
   {
     idle = false;
     brightness = DEFAULT_BRIGHTNESS;
@@ -242,18 +357,24 @@ void getDevicesData()
     lastClickTime.second = curTime.second;
     lastClickTime.day = curTime.day;
     lastClickTime.month = curTime.month;
+
+    if (alarmExecute)
+    {
+      alarmExecute = false;
+      alarmKilled = true;
+    }
   }
   if (brightness > IDLE_BRIGHTNESS)
   {
     int btnDiffMilis = GetSecsDiff(curTime, lastClickTime) * 1000;
-    if (btnDiffMilis > IDLE_MILIS)
+    if (btnDiffMilis > IDLE_MILIS && mode != MODE_FLASHLIGHT)
     {
       idle = true;
       brightness = IDLE_BRIGHTNESS;
     }
   }
 
-  if (btnBHold)
+  if (btnStatus[BTNB] == BTN_STATUS_HOLD)
   {
     holdSwitchTime.hour = curTime.hour;
     holdSwitchTime.minute = curTime.minute;
@@ -262,18 +383,13 @@ void getDevicesData()
     holdSwitchTime.month = curTime.month;
   }
 
-  if (btnAPressed) 
+  if (btnStatus[BTNA] == BTN_STATUS_HOLD || (mode != MODE_ROLL && btnStatus[BTNA] == BTN_STATUS_PRESSED)) 
   {
-    if (alarmExecute)
-    {
-      alarmExecute = false;
-      alarmKilled = true;
-    }
     setModeToNext();
   }
   if (mode == MODE_ALARM)
   {
-    if (btnBHold)
+    if (btnStatus[BTNB] == BTN_STATUS_HOLD)
     {
         alarmSetType = (alarmSetType + 1) % 3;
         if (alarmSetType == 2)
@@ -281,7 +397,7 @@ void getDevicesData()
           alarmEnable = !alarmEnable;
         }
     }
-    else if (btnBReleased)
+    else if (btnStatus[BTNB] == BTN_STATUS_RELEASED)
     {
       int milisDiff = GetSecsDiff(curTime, holdSwitchTime) * 1000;
       int minDiff = BTNB_HOLD_SWITCH_DELAY;
@@ -297,6 +413,47 @@ void getDevicesData()
         }
       }
     }
+  }
+  else if (mode == MODE_ROLL)
+  {
+    if (btnStatus[BTNB] == BTN_STATUS_RELEASED)
+    {
+      if (rollMode == 0)
+      {
+        incrementDiceType();
+      }
+      else if (rollMode == 1)
+      {
+        incrementDiceCount();
+      }
+      roll();
+    }
+    else if (btnStatus[BTNB] == BTN_STATUS_HOLD)
+    {
+      rollMode++;
+      if (rollMode > 1)
+      {
+        rollMode = 0;
+      }
+      roll();
+    }
+    else if(btnStatus[BTNA] == BTN_STATUS_PRESSED)
+    {
+      roll();
+    }
+    else if(btnStatus[BTNPWR] == BTN_STATUS_PRESSED)
+    {
+      roll();
+    }
+  }
+  else if (mode == MODE_FLASHLIGHT)
+  {
+    brightness = 255;
+  }
+  if (prevDeviceConnected != deviceConnected)
+  {
+    redraw = true;
+    prevDeviceConnected = deviceConnected;
   }
 }
 
@@ -352,6 +509,9 @@ void setMode()
       brightness = DEFAULT_BRIGHTNESS;
       case MODE_CLOCK:
         drawDelay = MODE_CLOCK_DRAW_DELAY;
+        clockColor = colors[getRandom(0, 8)];
+        StickCP2.Display.setTextColor(clockColor);
+        redraw = true;
         break;
       case MODE_FLASHLIGHT:
         drawDelay = MODE_FLASHLIGHT_DRAW_DELAY;
@@ -359,6 +519,12 @@ void setMode()
         break;
       case MODE_ALARM:
         drawDelay = MODE_ALARM_DRAW_DELAY;
+        break;
+      case MODE_ROLL:
+        drawDelay = DEFAULT_DRAW_DELAY;
+        diceCount = 2;
+        diceType = 6;
+        roll();
         break;
       default:
         drawDelay = DEFAULT_DRAW_DELAY;
@@ -426,6 +592,9 @@ void displayMode()
     case MODE_ALARM:
       displayAlarm();
       break;
+    case MODE_ROLL:
+      displayRoll();
+      break;
     default:
       resetMode();
       displayClockView(clockColor);
@@ -470,7 +639,7 @@ void waitTillNextFrame()
 
 void displayFlashlight(int milis)
 {
-  
+  brightness = 255;
   StickCP2.Display.fillRect(0, 0, StickCP2.Display.width(), StickCP2.Display.height(), WHITE);
 }
 
@@ -492,6 +661,11 @@ void displayClockView(int color)
     {
       StickCP2.Display.setCursor(20, 50);
       StickCP2.Display.printf("IDLE");
+    }
+    if (deviceConnected)
+    {
+      StickCP2.Display.setCursor(150, 20);
+      StickCP2.Display.printf("BLE");
     }
 
     StickCP2.Display.setTextSize(2.0f);
@@ -515,14 +689,103 @@ void setAlarm(int hour, int minute)
   alarmEnable = true;
 }
 
+void displayRoll()
+{
+  StickCP2.Display.clear();
+  StickCP2.Display.setTextSize(1.0f);
+
+  StickCP2.Display.setCursor(20, 20);
+  StickCP2.Display.printf("ROLL %dK%d", diceCount, diceType);
+
+  if (rollMode == 0)
+  {
+    StickCP2.Display.setCursor(150, 25);
+    StickCP2.Display.printf("_");
+  }
+  else
+  {
+    StickCP2.Display.setCursor(110, 25);
+    StickCP2.Display.printf("_");
+  }
+
+  StickCP2.Display.setTextSize(0.9f);
+  //StickCP2.Display.setCursor(170, 20);
+  //auto resultCount = sizeof(diceResult)/sizeof(diceResult[0]);
+  //int len = *(&diceResult + 1) - diceResult;
+  //StickCP2.Display.printf("(%d)", resultCount);
+
+  for (int i=0; i<diceCount; i++)
+  {
+    int result = diceResult[i];
+    if (result <= 0)
+    {
+      continue;
+    }
+    
+    StickCP2.Display.setCursor((35 * (i+1)) - 15, 50);
+    if (i == diceCount - 1)
+    {
+      StickCP2.Display.printf("%d", result);
+    }
+    else
+    {
+      StickCP2.Display.printf("%d,", result);
+    }
+  }
+  StickCP2.Display.setTextSize(2.5f);
+  StickCP2.Display.setCursor(30, 90);
+  StickCP2.Display.printf("= %d", diceSum);
+  StickCP2.Display.setTextSize(1.0f);
+}
+
+void incrementDiceType()
+{
+  int oldType = diceType;
+
+  for (int i=0; i<DICE_TYPES_COUNT; i++)
+  {
+    if (diceTypes[i] > diceType)
+    {
+      diceType = diceTypes[i];
+      break;
+    }
+  }
+
+  if (diceType == oldType)
+  {
+    diceType = diceTypes[0];
+  }
+
+  clearDiceResults();
+}
+
+void incrementDiceCount()
+{
+  diceCount++;
+  if (diceCount > DICE_COUNT_MAX)
+  {
+    diceCount = 1;
+  }
+  clearDiceResults();
+}
+
 void roll()
 {
-  diceResult = new unsigned int(diceCount);
+  clearDiceResults();
   diceSum = 0;
-  for (unsigned int i=0; i<diceCount; i++)
+  for (int i=0; i<diceCount; i++)
   {
     diceResult[i] = (rand() % diceType) + 1;
     diceSum += diceResult[i];
+  }
+}
+
+void clearDiceResults()
+{
+  diceResult = new int[6] {0,0,0,0,0,0}; //new int(diceCount);
+  for (int i=0; i<DICE_COUNT_MAX; i++)
+  {
+    diceResult[i] = 0;
   }
 }
 
